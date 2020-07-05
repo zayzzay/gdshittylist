b3ec91156160af78a7ea98c5217d64f7cc628ffc
monstergamer546 <67807137+monstergamer546@users.noreply.github.com>
2020-07-04 22:58:41 -0400

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1278,7 +1278,14 @@ const d = {
 					"percent": 100,
 					"hz": "300hz"
 				},
-			],
+			        {
+    "user": "PriUsGD",
+    "link": "https://youtu.be/YlaehvdmMLU",
+    "percent": 100,
+    "hz": "144hz"
+},
+
+                        ],
 			"name": "Shitty Rate Demon",
 			"author": "Serpyy",
 			"more": "none",
