f1506644a4d762f9ad269509717b404e9088e9c9
N1kix08 <68759634+N1kix08@users.noreply.github.com>
2020-07-26 01:46:58 +0300

    Update list.js

--- js/list.js
+++ js/list.js
@@ -131,6 +131,12 @@ const d = {
 					"percent": 100,
 					"hz": "144hz"
 				},
+				{
+					"user": "Magma",
+					"link": "https://www.youtube.com/watch?v=FaIvB3wRWY0&feature=youtu.be",
+					"percent": 100,
+					"hz": "300hz"
+				},
 			],
 			"name": "Shitty Kowareta",
 			"author": "LJosh",
