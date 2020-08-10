ac2e97511da7fb36a5c558e158cf255489c85207
piesyy <69423933+piesyy@users.noreply.github.com>
2020-08-10 18:26:56 -0400

    Update list.js

--- js/list.js
+++ js/list.js
@@ -12310,6 +12310,12 @@ const d = {
 					"percent": 100,
 					"hz": "Mobile"
 				},
+				{
+					"user": "bluey",
+					"link": "https://www.youtube.com/watch?v=-iLz0GyQ6_4",
+					"percent": 100,
+					"hz": "240hz"
+				},
                         ],
 			"name": "Shitty Killerzone",
 			"author": "Acropolisboy",
