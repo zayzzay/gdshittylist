a0da1aabeac4ed331193a1af392b2a88c73f2dee
N1kix08 <68759634+N1kix08@users.noreply.github.com>
2020-08-11 19:21:46 +0300

    Update list.js

--- js/list.js
+++ js/list.js
@@ -93,6 +93,12 @@ const d = {
 				       "percent": 100,
 				       "hz": "240hz"
 			       },
+			       {
+				       "user": "Ryth",
+				       "link": "https://www.youtube.com/watch?v=YUoseOc7O0w&feature=youtu.be",
+				       "percent": 100,
+				       "hz": "288hz"
+			       },
                        ],
                        "name": "Shitty Azure Flare",
                        "author": "Magma",
