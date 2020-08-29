cc4033b6dd9007ea62f6557fc2edc917e7929f99
Aassbll11 <68526047+Aassbll11@users.noreply.github.com>
2020-08-29 11:09:51 +0800

    Update list.js

--- js/list.js
+++ js/list.js
@@ -14022,6 +14022,12 @@ const d = {
 					"percent": 100,
 					"hz": "144hz"
 				},
+				{
+					"user": "GD Cade21",
+					"link": "https://www.youtube.com/watch?v=Sds7ncwRgIM",
+					"percent": 100,
+					"hz": "144hz"
+				},
 			],
 			"name": "Shitty Lucid Nightmares",
 			"author": "ShadowNinja441 and more [ShadowNinja441]",
